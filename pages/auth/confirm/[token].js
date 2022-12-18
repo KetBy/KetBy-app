d946def868553a14f2fd31e8c61394fa0d81dd95
alexhodo <alexandru_hodorogea2001@yahoo.com>
2022-12-18 23:26:43 +0100

    auth/confirm page CSR

--- pages/auth/confirm/[token].js
+++ pages/auth/confirm/[token].js
@@ -1,14 +1,43 @@
 import * as React from "react";
-import { Box, Alert, Button, Grid } from "@mui/material";
+import { Box, Alert, Button, Grid, Typography } from "@mui/material";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import axios from "../../../src/utils/axios";
 import theme from "../../../src/themes/default";
+import CustomCircularProgress from "../../../src/components/custom/CircularProgress";
 
-export default function AuthConfirmPage({ data }) {
+export default function AuthConfirmPage() {
+  const router = useRouter();
+  const { token } = router.query;
+
+  const [loaded, setLoaded] = React.useState(false);
+  const [data, setData] = React.useState({
+    success: null,
+    message: null,
+  });
+
+  React.useEffect(() => {
+    axios
+      .post("/auth/confirm", {
+        token: token,
+      })
+      .then((res) => {
+        setData(res.data);
+        setLoaded(true);
+      })
+      .catch((err) => {
+        setData({
+          success: false,
+          message: "Something went wrong. Please try again later.",
+        });
+        setLoaded(true);
+      });
+  }, []);
+
   return (
     <>
       <Head>
@@ -16,8 +45,6 @@ export default function AuthConfirmPage({ data }) {
       </Head>
       <Box
         sx={{
-          width: "auto",
-          maxWidth: { xs: "auto", md: "300px" },
           minHeight: `calc(100vh - ${theme.constants.menuHeight}px)`,
           display: "grid",
           alignItems: "center",
@@ -26,61 +53,68 @@ export default function AuthConfirmPage({ data }) {
           px: 3,
         }}
       >
-        <Box
-          sx={{
-            width: "auto",
-            maxWidth: { xs: "auto", md: "300px" },
-            minWidth: { md: "300px" },
-          }}
-        >
-          <Alert
-            severity={data.success ? "success" : "warning"}
-            sx={{ mb: 2, border: `1px solid ${theme.palette.grey.main}` }}
+        {loaded ? (
+          <Box
+            sx={{
+              width: "auto",
+              maxWidth: { xs: "auto", md: "300px" },
+              minWidth: { md: "300px" },
+            }}
           >
-            {data.message}
-          </Alert>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <Button
-                variant="outlined"
-                fullWidth
-                startIcon={<HomeRoundedIcon />}
-                component={Link}
-                href="/"
-              >
-                Back home
-              </Button>
+            <Alert
+              severity={data.success ? "success" : "warning"}
+              sx={{ mb: 2, border: `1px solid ${theme.palette.grey.main}` }}
+            >
+              {data.message}
+            </Alert>
+            <Grid container spacing={2} rowSpacing={2}>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="outlined"
+                  fullWidth
+                  startIcon={<HomeRoundedIcon />}
+                  component={Link}
+                  href="/"
+                >
+                  Back home
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  endIcon={
+                    data.success ? <LoginRoundedIcon /> : <PersonRoundedIcon />
+                  }
+                  component={Link}
+                  href={data.success ? "/auth/login" : "/auth/register"}
+                  sx={{ display: { xs: "none", sm: "flex" } }}
+                >
+                  {data.success ? "Log in" : "Register"}
+                </Button>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  startIcon={
+                    data.success ? <LoginRoundedIcon /> : <PersonRoundedIcon />
+                  }
+                  component={Link}
+                  href={data.success ? "/auth/login" : "/auth/register"}
+                  sx={{ display: { xs: "default", sm: "none" } }}
+                >
+                  {data.success ? "Log in" : "Register"}
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Button
-                variant="contained"
-                fullWidth
-                endIcon={
-                  data.success ? <LoginRoundedIcon /> : <PersonRoundedIcon />
-                }
-                component={Link}
-                href={data.success ? "/auth/login" : "/auth/register"}
-              >
-                {data.success ? "Log in" : "Register"}
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        ) : (
+          <Box sx={{ textAlign: "center" }}>
+            <CustomCircularProgress />
+
+            <Typography variant="body1">Please wait a second...</Typography>
+          </Box>
+        )}
       </Box>
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  const { token } = context.query;
-
-  const res = await axios.post("/auth/confirm", {
-    token: token,
-  });
-
-  const data = res.data;
-
-  return {
-    props: { data },
-  };
 }
---
+++ src/components/custom/CircularProgress.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CircularProgress, {
+  circularProgressClasses,
+} from "@mui/material/CircularProgress";
+
+// Inspired by the former Facebook spinners.
+export default function CustomCircularProgress(props) {
+  return (
+    <Box sx={{ position: "relative", display: "inline" }}>
+      <CircularProgress
+        variant="determinate"
+        sx={{
+          color: (theme) => theme.palette.grey[200],
+        }}
+        size={40}
+        thickness={4}
+        {...props}
+        value={100}
+      />
+      <CircularProgress
+        variant="indeterminate"
+        disableShrink
+        sx={{
+          color: (theme) => theme.palette.primary.main,
+          animationDuration: "550ms",
+          position: "absolute",
+          left: 0,
+          [`& .${circularProgressClasses.circle}`]: {
+            strokeLinecap: "round",
+          },
+        }}
+        size={40}
+        thickness={4}
+        {...props}
+      />
+    </Box>
+  );
+}
