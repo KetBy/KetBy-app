c0f0bfb340f894d16cfeb8401539e19276d78a10
alexhodo <alexandru_hodorogea2001@yahoo.com>
2022-12-18 22:49:20 +0100

    Page titles

--- pages/auth/confirm/[token].js
+++ pages/auth/confirm/[token].js
@@ -3,62 +3,71 @@ import { Box, Alert, Button, Grid } from "@mui/material";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import Head from "next/head";
 import Link from "next/link";
 import axios from "../../../src/utils/axios";
 import theme from "../../../src/themes/default";
 
 export default function AuthConfirmPage({ data }) {
   return (
-    <Box
-      sx={{
-        minHeight: `calc(100vh - ${theme.constants.menuHeight}px)`,
-        display: "grid",
-        alignItems: "center",
-        justifyContent: "center",
-        py: 5,
-      }}
-    >
+    <>
+      <Head>
+        <title>Confirm your account | KetBy</title>
+      </Head>
       <Box
         sx={{
           width: "auto",
-
           maxWidth: { xs: "auto", md: "300px" },
+          minHeight: `calc(100vh - ${theme.constants.menuHeight}px)`,
+          display: "grid",
+          alignItems: "center",
+          justifyContent: "center",
+          py: 5,
+          px: 3,
         }}
       >
-        <Alert
-          severity={data.success ? "success" : "warning"}
-          sx={{ mb: 2, border: `1px solid ${theme.palette.grey.main}` }}
+        <Box
+          sx={{
+            width: "auto",
+            maxWidth: { xs: "auto", md: "300px" },
+            minWidth: { md: "300px" },
+          }}
         >
-          {data.message}
-        </Alert>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <Button
-              variant="outlined"
-              fullWidth
-              startIcon={<HomeRoundedIcon />}
-              component={Link}
-              href="/"
-            >
-              Back home
-            </Button>
+          <Alert
+            severity={data.success ? "success" : "warning"}
+            sx={{ mb: 2, border: `1px solid ${theme.palette.grey.main}` }}
+          >
+            {data.message}
+          </Alert>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Button
+                variant="outlined"
+                fullWidth
+                startIcon={<HomeRoundedIcon />}
+                component={Link}
+                href="/"
+              >
+                Back home
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                fullWidth
+                endIcon={
+                  data.success ? <LoginRoundedIcon /> : <PersonRoundedIcon />
+                }
+                component={Link}
+                href={data.success ? "/auth/login" : "/auth/register"}
+              >
+                {data.success ? "Log in" : "Register"}
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Button
-              variant="contained"
-              fullWidth
-              endIcon={
-                data.success ? <LoginRoundedIcon /> : <PersonRoundedIcon />
-              }
-              component={Link}
-              href={data.success ? "/auth/login" : "/auth/register"}
-            >
-              {data.success ? "Log in" : "Register"}
-            </Button>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 
--- pages/composer/index.js
+++ pages/composer/index.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 import { Box } from "@mui/material";
+import Head from "next/head";
 import Composer from "../../src/components/Composer/index.js";
 
 export default function ComposerPage() {
   return (
     <>
+      <Head>
+        <title>Quantum composer | KetBy</title>
+      </Head>
       <Composer />
     </>
   );
