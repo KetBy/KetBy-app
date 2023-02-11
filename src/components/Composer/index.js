28ce20f39478257bb0bf8ef3685f29d5750903be
alexhodo <alexandru_hodorogea2001@yahoo.com>
2023-02-11 16:19:24 +0100

    Update index.js

--- src/components/Composer/index.js
+++ src/components/Composer/index.js
@@ -63,75 +63,77 @@ export default function Composer(props) {
   const [activeFile, setActiveFile] = React.useState(null);
 
   React.useEffect(() => {
-    if (preview === true) {
-      setProject({
-        id: 1,
-        title: "Demo project",
-        description: "This is just a demo quantum project",
-        thumbnail_url: null,
-        public: 1,
-        forked_from: null,
-        forks_count: 0,
-        stars_count: 0,
-        next_file_index: 2,
-        token: "-",
-      });
-      setFiles({
-        1: {
+    setTimeout(() => {
+      if (preview === true) {
+        setProject({
           id: 1,
-          project_id: 1,
-          file_index: 1,
-          title: "New file",
-          content:
-            "S[0]; H[1]; T+[2]; I[2]; CX[0,2]; X[1]; SX[2]; Tfl[1,0,3]; X[2]; I[0]; I[1]; I[1]; SWAP[1,3]; SX+[1]; X[0]; X[0]; S+[0]; RX[3]; H[0]; CX[2,0]",
-          file_type_name: "qc",
-          meta: {
-            qubits: 5,
-            bits: 0,
+          title: "Demo project",
+          description: "This is just a demo quantum project",
+          thumbnail_url: null,
+          public: 1,
+          forked_from: null,
+          forks_count: 0,
+          stars_count: 0,
+          next_file_index: 2,
+          token: "-",
+        });
+        setFiles({
+          1: {
+            id: 1,
+            project_id: 1,
+            file_index: 1,
+            title: "New file",
+            content:
+              "S[0]; H[1]; T+[2]; I[2]; CX[0,2]; X[1]; SX[2]; Tfl[1,0,3]; X[2]; I[0]; I[1]; I[1]; SWAP[1,3]; SX+[1]; X[0]; X[0]; S+[0]; RX[3]; H[0]; CX[2,0]",
+            file_type_name: "qc",
+            meta: {
+              qubits: 5,
+              bits: 0,
+            },
           },
-        },
-      });
-      setActiveFile(1);
-      setLoading(false);
-    } else {
-      if (projectToken && fileIndex) {
-        axios
-          .get(`/project/${projectToken}`, {
-            fileIndex: parseInt(fileIndex),
-          })
-          .then((res) => {
-            let data = res.data;
-            if (data.success) {
-              setProject(data.project);
-              let newFiles = {};
-              data.files.map((file, index) => {
-                newFiles[file.file_index] = file;
-              });
-              setFiles(newFiles);
-              if (typeof newFiles[fileIndex] == "undefined") {
-                throw new Exception("This file does not exist");
-              }
-              setActiveFile(fileIndex);
-              setLoading(false);
-            } else {
-              throw new Exception("Something went wrong.");
-            }
-          })
-          .catch((err) => {
-            setError(err.message);
-            console.log(err);
-          });
+        });
+        setActiveFile(1);
+        setLoading(false);
       } else {
-        let startCircuit = {
-          meta: {
-            qubits: 1,
-            bits: 0,
-          },
-          instructions: [],
-        };
-        setCircuit(startCircuit);
+        if (projectToken && fileIndex) {
+          axios
+            .get(`/project/${projectToken}`, {
+              fileIndex: parseInt(fileIndex),
+            })
+            .then((res) => {
+              let data = res.data;
+              if (data.success) {
+                setProject(data.project);
+                let newFiles = {};
+                data.files.map((file, index) => {
+                  newFiles[file.file_index] = file;
+                });
+                setFiles(newFiles);
+                if (typeof newFiles[fileIndex] == "undefined") {
+                  throw new Exception("This file does not exist");
+                }
+                setActiveFile(fileIndex);
+                setLoading(false);
+              } else {
+                throw new Exception("Something went wrong.");
+              }
+            })
+            .catch((err) => {
+              setError(err.message);
+              console.log(err);
+            });
+        } else {
+          let startCircuit = {
+            meta: {
+              qubits: 1,
+              bits: 0,
+            },
+            instructions: [],
+          };
+          setCircuit(startCircuit);
+        }
       }
-    }
+    }, 300);
   }, []);
 
   return (
